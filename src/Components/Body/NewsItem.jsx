46eab594ff8bbf90ecf68dc5914ab1d3cb56a761
sheikh295 <sheikh295@users.noreply.github.com>
2023-08-28 12:52:08 +0500

    .

--- src/Components/Body/NewsItem.jsx
+++ src/Components/Body/NewsItem.jsx
@@ -15,7 +15,7 @@ function NewsItem(props) {
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href={newsSrc} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                <a href={newsSrc} target='_blank' className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
       </div>
